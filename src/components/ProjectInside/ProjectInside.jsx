2fa0ed51f1a5316dd953010ca38a284fb7e9c5e6
Altuğ Altuner <vefgend@gmail.com>
2024-05-20 21:30:55 +0300

    serverside

--- src/components/ProjectInside/ProjectInside.jsx
+++ src/components/ProjectInside/ProjectInside.jsx
@@ -1,20 +1,36 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import "./ProjectInside.scss";
 
-import projectList from "../../database/companyProjects.js/projectList";
-
 function ProjectInside() {
+
+    const [projects, setProjects] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('http://localhost:1337/api/projects');
+                console.log(response.data); // Yanıtı kontrol etmek için konsola yazdırın
+                setProjects(response.data.data); // API'nin döndürdüğü veriyi kontrol edin ve uygun şekilde kaydedin
+            } catch (error) {
+                console.error('Error fetching the data', error);
+            }
+        };
+
+        fetchData();
+    }, []);
 
     return (
         <div className="project-inside-main">
             <div className="projects-sidebar">
                 <h2 className="sidebar-title">Projeler</h2>
                 <ul className="projects-list">
-                    {projectList.map((project) => (
-                        <li key={project.name} className="project-item">
-                            <img className="project-navbar-photos" src={project.photo} alt={project.name} />
-                            <p className="project-navbar-name">{project.name}</p>
+                    {projects.length > 0 ? projects.map(project => (
+                        <li key={project.id} className="project-item">
+                            <img className="project-navbar-photos" src={project.projectCoverPhoto} alt={project.projectName} />
+                            {project.attributes.projectName}
                         </li>
-                    ))}
+                    )) : <li>Yükleniyor</li>}
                 </ul>
             </div>
         </div>
