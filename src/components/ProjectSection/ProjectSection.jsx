c77a4758a3d8783107319e295bf6e42054075b31
Altuğ Altuner <vefgend@gmail.com>
2024-05-21 16:09:13 +0300

    edit

--- src/components/ProjectSection/ProjectSection.jsx
+++ src/components/ProjectSection/ProjectSection.jsx
@@ -1,23 +1,41 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from 'axios';
 import "./ProjectSection.scss";
-import projectFolders from "../../database/projectFolders";
 
 function ProjectSection({ onItemClick }) {
+
+    const [projectFolders, setProjectFolders] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('http://localhost:1337/api/project-folders?populate=projectfolderimage');
+                console.log(response.data); // Yanıtı kontrol etmek için konsola yazdırın
+                setProjectFolders(response.data.data); // API'nin döndürdüğü veriyi kontrol edin ve uygun şekilde kaydedin
+            } catch (error) {
+                console.error('Error fetching the data', error);
+            }
+        };
+
+        fetchData();
+    }, []);
 
     return (
         <div className="project-sections">
             <h2 className="section-header">Proje Dosyaları</h2>
-            {projectFolders.map((section, index) => (
-                <div key={index} className="project-section-one" onClick={() => onItemClick(section.title)}>
-                    <h3 className="project-section-title">{section.title}</h3>
+            {projectFolders.map((folder) => (
+                <div key={folder.id} className="project-section-one" onClick={() => onItemClick(folder)}>
+                    <h3 className="project-section-title">{folder.attributes.projectFolderName}</h3>
                     <div className="project-images-container">
-                        {section.images.map((image, idx) => (
-                            <img key={idx} src={image} alt={section.title} className="project-image" />
-                        ))}
+                        <img className="project-image"
+                            src={`http://localhost:1337${folder.attributes.projectfolderimage.data.attributes.url}`}
+                            alt={folder.attributes.projectFolderName}
+                        />
                     </div>
                 </div>
             ))}
         </div>
     );
 }
+
 export default ProjectSection;
