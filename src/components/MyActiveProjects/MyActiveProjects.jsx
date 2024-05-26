5fd54c5d6dd83d442fd4eb997f6e0574bcaf61b8
Altuğ Altuner <vefgend@gmail.com>
2024-05-26 17:38:24 +0300

    unnecessary files are deleted

--- src/components/MyActiveProjects/MyActiveProjects.jsx
+++ src/components/MyActiveProjects/MyActiveProjects.jsx
@@ -1,8 +1,6 @@
 import React, { useState } from "react";
 import "./MyActiveProjects.scss";
 
-import projectList from "../../database/companyProjects.js/projectList.js";
-
 function MyActiveProjects() {
 
     return (
@@ -10,12 +8,7 @@ function MyActiveProjects() {
             <div className="active-projects">
                 <h1 className="active-projects-header">My Active Projects</h1>
                 <div className="active-projects-container">
-                    {projectList.map((project, index) => (
-                        <div className="active-project" key={index}>
-                            <div className="project-name">{project.name}</div>
-                            <img className="project-photo-active" src={project.photo} alt={project.alt} />
-                        </div>
-                    ))}
+
                 </div>
             </div>
         </div>
