dad2e1c62583098abf19f4277e81a6aa1185330f
Altuğ Altuner <vefgend@gmail.com>
2024-05-30 15:43:32 +0300

    group page installation

--- src/pages/GroupsPage/GroupsPage.jsx
+++ src/pages/GroupsPage/GroupsPage.jsx
@@ -7,6 +7,23 @@ function GroupsPage() {
         <div className="groups-main">
             <Navigation />
             <h1>Groups</h1>
+            <div className="project-groups">
+                <div className="group">
+                    <h2>Group 1</h2>
+                    <p>Members: 1, 2, 3</p>
+                    <p>Project: Project 1</p>
+                </div>
+                <div className="group">
+                    <h2>Group 2</h2>
+                    <p>Members: 4, 5, 6</p>
+                    <p>Project: Project 2</p>
+                </div>
+                <div className="group">
+                    <h2>Group 3</h2>
+                    <p>Members: 7, 8, 9</p>
+                    <p>Project: Project 3</p>
+                </div>
+            </div>
 
         </div>
     );
