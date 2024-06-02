ebabbb360e3e1ee8e263a6b1e5f67d3552a566e2
Altuğ Altuner <vefgend@gmail.com>
2024-06-02 14:13:06 +0300

    commit

--- src/pages/GroupsPage/GroupsPage.jsx
+++ src/pages/GroupsPage/GroupsPage.jsx
@@ -22,40 +22,6 @@ function GroupsPage() {
         fetchGroups();
     }, []);
 
-    const settings = {
-        dots: true,
-        infinite: false,
-        speed: 500,
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        responsive: [
-            {
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 1,
-                    infinite: true,
-                    dots: true
-                }
-            },
-            {
-                breakpoint: 600,
-                settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 1,
-                    initialSlide: 2
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                }
-            }
-        ]
-    };
-
     return (
         <div className="groups-main">
             <Navigation />
