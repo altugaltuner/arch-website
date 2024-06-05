b88181477501b1f2b566ee66ec670d159aac055e
Altuğ Altuner <vefgend@gmail.com>
2024-06-05 16:42:07 +0300

    db integration succesfull

--- src/pages/ProjectsMainPage/ProjectsMainPage.jsx
+++ src/pages/ProjectsMainPage/ProjectsMainPage.jsx
@@ -1,15 +1,35 @@
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import "./ProjectsMainPage.scss";
+import ProjectInside from "../../components/ProjectInside/ProjectInside";
+import Navigation from "../../components/Navigation/Navigation";
+import ProjectHeader from "../../components/ProjectHeader/ProjectHeader";
+import ProjectContent from "../../components/ProjectContent/ProjectContent";
 
 function ProjectsMainPage() {
 
     const [companyProjects, setCompanyProjects] = useState([]);
+    const [roles, setRoles] = useState([]);
+
+    async function getRoles() {
+        try {
+            const response = await axios.get('http://localhost:1337/api/accesses');
+            setRoles(response.data.data);
+        }
+        catch (error) {
+            console.error(error);
+        }
+    }
+
+    useEffect(() => {
+        getRoles();
+    }, []);
 
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await axios.get('http://localhost:1337/api/projects');
+                const response = await axios.get('http://localhost:1337/api/projects?populate=projectCoverPhoto');
                 setCompanyProjects(response.data.data);
             } catch (error) {
                 console.error('Error fetching the data', error);
@@ -21,43 +41,31 @@ function ProjectsMainPage() {
 
     return (
         <div>
-            {companyProjects.map((project) => (
-                <div className="project-card" key={project.id}>
-                    <Link to={`/projects/${project.id}`}>{project.attributes.projectName}</Link>
-                </div>
-            ))}
-
             <div className="projects-main-page">
-                {roles.map(role => {
-                    if (role.attributes.role === "Admin") {
-                        return (
-
-                            <button>
-                                proje ekle
-                            </button>
-                        )
-                    }
-                }
-                )}
                 <Navigation />
-                <div className="inner-project-page">
-                    <ProjectInside onProjectClick={handleProjectClick} />
-                    <div className="inner-project-column">
-                        <ProjectHeader clickedProject={selectedProject} onTabChange={handleTabChange} />
-                        {selectedItem ? (
-                            <ProjectDetails
-                                selectedItem={selectedItem}
-                                activeFolder={activeFolder}
-                                companyProjects={companyProjects}
-                                selectedProject={selectedProject}
-                                onBack={() => setSelectedItem(null)}
-                            />
-                        ) : (
-                            <ProjectContent currentTab={currentTab} handleItemClick={handleItemClick} />
-                        )}
+                {companyProjects.map((project) => (
+                    <div className="project-cards" key={project.id}>
+                        <div className="project-card">
+                            <Link to={`/projects/${project.id}`}>
+                                <img className="project-navbar-photos" src={`http://localhost:1337${project.attributes.projectCoverPhoto.data.attributes.url}`} alt="project-photo" />
+                                {project.attributes.projectName}
+                            </Link>
+                        </div>
+
                     </div>
-                </div>
+                ))}
             </div>
+            {roles.map(role => {
+                if (role.attributes.role === "Admin") {
+                    return (
+
+                        <button>
+                            proje ekle
+                        </button>
+                    )
+                }
+            }
+            )}
         </div>
     );
 }
