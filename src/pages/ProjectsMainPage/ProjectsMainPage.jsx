5497135fef265cdb6bc9ea0eec4dd29b50d73fb6
Altuğ Altuner <vefgend@gmail.com>
2024-06-05 16:30:07 +0300

    new page

--- src/pages/ProjectsMainPage/ProjectsMainPage.jsx
+++ src/pages/ProjectsMainPage/ProjectsMainPage.jsx
@@ -1,36 +1,63 @@
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 function ProjectsMainPage() {
-    const projects = [
-        {
-            id: 1,
-            name: "Bişey Konutları",
-            description: "Description1",
-        },
-        {
-            id: 2,
-            name: "Project2",
-            description: "Description2",
-        },
-        {
-            id: 3,
-            name: "Project3",
-            description: "Description3",
-        },
-        {
-            id: 4,
-            name: "Project4",
-            description: "Description4",
-        },
-    ];
+
+    const [companyProjects, setCompanyProjects] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get('http://localhost:1337/api/projects');
+                setCompanyProjects(response.data.data);
+            } catch (error) {
+                console.error('Error fetching the data', error);
+            }
+        };
+
+        fetchData();
+    }, []);
 
     return (
         <div>
-            {projects.map((project) => (
+            {companyProjects.map((project) => (
                 <div className="project-card" key={project.id}>
-                    <Link to={`/projects/${project.id}`}>{project.name}</Link>
+                    <Link to={`/projects/${project.id}`}>{project.attributes.projectName}</Link>
                 </div>
             ))}
+
+            <div className="projects-main-page">
+                {roles.map(role => {
+                    if (role.attributes.role === "Admin") {
+                        return (
+
+                            <button>
+                                proje ekle
+                            </button>
+                        )
+                    }
+                }
+                )}
+                <Navigation />
+                <div className="inner-project-page">
+                    <ProjectInside onProjectClick={handleProjectClick} />
+                    <div className="inner-project-column">
+                        <ProjectHeader clickedProject={selectedProject} onTabChange={handleTabChange} />
+                        {selectedItem ? (
+                            <ProjectDetails
+                                selectedItem={selectedItem}
+                                activeFolder={activeFolder}
+                                companyProjects={companyProjects}
+                                selectedProject={selectedProject}
+                                onBack={() => setSelectedItem(null)}
+                            />
+                        ) : (
+                            <ProjectContent currentTab={currentTab} handleItemClick={handleItemClick} />
+                        )}
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
