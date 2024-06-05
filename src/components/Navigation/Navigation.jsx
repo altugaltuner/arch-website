c9b73908eddf4fd9f8c07737108279a9359c239b
Altuğ Altuner <vefgend@gmail.com>
2024-06-05 18:58:54 +0300

    gereksizler silindi

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,6 +1,5 @@
-import { useState } from "react";
 import "./Navigation.scss";
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function Navigation() {
 
