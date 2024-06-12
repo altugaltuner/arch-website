fb888860da9279faaf8223cc02eb1e32a2a338e9
Altuğ Altuner <vefgend@gmail.com>
2024-06-12 12:15:05 +0300

    çalışır hal

--- src/pages/WorkersPage/WorkersPage.jsx
+++ src/pages/WorkersPage/WorkersPage.jsx
@@ -8,13 +8,6 @@ function WorkersPage() {
     const [selectedEmployee, setSelectedEmployee] = useState(null);
     const [selectedJobTitle, setSelectedJobTitle] = useState('Tümü');
     const [jobTitles, setJobTitles] = useState([]);
-    const [roles, setRoles] = useState([]);
-    const [showAddingModal, setShowAddingModal] = useState(false);
-    const [newEmployee, setNewEmployee] = useState({
-        username: "",
-        email: "",
-        profilePic: null
-    });
 
     const sendEmail = (email) => {
         window.location.href = `mailto:${email}`;
@@ -24,27 +17,12 @@ function WorkersPage() {
         setSelectedJobTitle(title);
     };
 
-    async function getRoles() {
-        try {
-            const response = await axios.get('http://localhost:1337/api/accesses');
-            setRoles(response.data.data);
-        } catch (error) {
-            console.error(error);
-        }
-    }
-
-    useEffect(() => {
-        getRoles();
-    }, []);
-
     useEffect(() => {
         const fetchData = async () => {
             try {
                 const response = await axios.get('http://localhost:1337/api/users?populate=profession,projects,profilePic');
-                console.log(response.data); // Log the response to check the data
-                setEmployees(response.data); // Check the structure of the response and adjust accordingly
-
-                // Extract unique job titles
+                console.log(response.data);
+                setEmployees(response.data);
                 const titles = response.data.map(employee => employee.profession.professionName);
                 const uniqueTitles = Array.from(new Set(titles));
                 setJobTitles(uniqueTitles);
@@ -63,35 +41,6 @@ function WorkersPage() {
 
     const closeEmployeeCardModal = () => {
         setSelectedEmployee(null);
-    };
-
-
-    const handleInputChange = (e) => {
-        const { name, value } = e.target;
-        setNewEmployee({ ...newEmployee, [name]: value });
-    };
-
-    const handleFileChange = (e) => {
-        setNewEmployee({ ...newEmployee, profilePic: e.target.files[0] });
-    };
-
-    const handleSubmit = async () => {
-        const formData = new FormData();
-        formData.append('data', JSON.stringify({ username: newEmployee.username, email: newEmployee.email }));
-        if (newEmployee.profilePic) {
-            formData.append('files.profilePic', newEmployee.profilePic);
-        }
-
-        try {
-            await axios.post('http://localhost:1337/api/users', formData);
-            setShowAddingModal(false);
-            setNewEmployee({ username: "", email: "", profilePic: null });
-            // Refetch employees after adding a new one
-            const response = await axios.get('http://localhost:1337/api/users?populate=profession,projects,profilePic');
-            setEmployees(response.data);
-        } catch (error) {
-            console.error('Error creating a new employee', error);
-        }
     };
 
     const filteredEmployees = selectedJobTitle === 'Tümü'
@@ -122,16 +71,6 @@ function WorkersPage() {
                     </ul>
                 </div>
                 <div className="employee-grid">
-
-                    {roles.map(role => role.attributes.role === "Admin" && (
-                        <button
-                            className="add-employee-btn"
-                            onClick={() => setShowAddingModal(true)}
-                        >
-                            Çalışan Ekle
-                        </button>
-                    ))}
-
                     {filteredEmployees.map((employee, index) => (
                         <div className="employee-card" key={index} onClick={() => openEmployeeCardModal(employee)}>
                             <div className="profile-pic">
@@ -149,7 +88,7 @@ function WorkersPage() {
                     <div className="employee-card-modal">
                         <div className="employee-card-modal-inner">
                             <div className="profile-pic">
-                                <img className="profile-pic-inner" src={`http://localhost:1337${selectedEmployee.profilePic.url}`} alt="" srcSet="" />
+                                <img className="profile-pic-inner" src={`http://localhost:1337${selectedEmployee.profilePic?.url || ""}`} alt="" srcSet="" />
                             </div>
                             <p>{selectedEmployee.username}</p>
                             <p>{selectedEmployee.email}</p>
@@ -160,39 +99,6 @@ function WorkersPage() {
                     </div>
                 )}
             </div>
-
-            {showAddingModal && (
-                <div className="add-new-employee-modal">
-                    <div className="new-employee-modal-content">
-                        <span className="new-employee-modal-close" onClick={() => setShowAddingModal(false)}>X</span>
-                        <h2 className="new-employee-modal-header">Yeni Çalışan Ekle</h2>
-                        <input className="employee-name-input"
-                            type="text"
-                            name="username"
-                            placeholder="Çalışan Adı"
-                            value={newEmployee.username}
-                            onChange={handleInputChange}
-                        />
-                        <input className="employee-email-input"
-                            type="email"
-                            name="email"
-                            placeholder="Email"
-                            value={newEmployee.email}
-                            onChange={handleInputChange}
-                        />
-                        <input className="new-employee-profile-pic"
-                            type="file"
-                            name="profilePic"
-                            onChange={handleFileChange}
-                        />
-                        <div className="adding-modal-buttons-row">
-                            <button className="adding-modal-button-create" onClick={handleSubmit}>Oluştur</button>
-                            <button className="adding-modal-button-abort" onClick={() => setShowAddingModal(false)}>İptal</button>
-                        </div>
-                    </div>
-                </div>
-            )}
-
         </div>
     );
 }
