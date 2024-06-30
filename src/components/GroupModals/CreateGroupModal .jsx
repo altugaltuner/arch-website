2d407b07a5827a55920f02e41a94b6c02915de90
Altuğ Altuner <vefgend@gmail.com>
2024-06-30 14:48:55 +0300

    m

--- src/components/GroupModals/CreateGroupModal .jsx
+++ src/components/GroupModals/CreateGroupModal .jsx
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './CreateGroupModal.scss';
 
 const CreateGroupModal = ({ showModal, setShowModal, newGroup, handleInputChange, handleSubmit }) => {
+    const [errorMessage, setErrorMessage] = useState('');
+
     if (!showModal) return null;
+
+    const handleGroupNameChange = (e) => {
+        const { value } = e.target;
+        if (value.length <= 40) {
+            handleInputChange(e);
+            setErrorMessage(''); // Clear the error message if within limit
+        } else {
+            setErrorMessage('Grup adı en fazla 40 karakter olabilir.');
+        }
+    };
+
+    const handleFormSubmit = () => {
+        if (newGroup.groupName.length > 40) {
+            setErrorMessage('Grup adı en fazla 40 karakter olabilir.');
+            return null;
+        }
+        handleSubmit();
+    };
 
     return (
         <div className="create-group-modal">
@@ -15,10 +35,11 @@ const CreateGroupModal = ({ showModal, setShowModal, newGroup, handleInputChange
                     name="groupName"
                     placeholder="Grup Adı"
                     value={newGroup.groupName}
-                    onChange={handleInputChange}
+                    onChange={handleGroupNameChange}
                 />
+                {errorMessage && <p className='error-message'>{errorMessage}</p>}
                 <div className='create-group-button-div-group'>
-                    <button className='create-group-modal-div-create-btn' onClick={handleSubmit}>Oluştur</button>
+                    <button className='create-group-modal-div-create-btn' onClick={handleFormSubmit}>Oluştur</button>
                     <button className='create-group-modal-div-cancel-btn' onClick={() => setShowModal(false)}>İptal</button>
                 </div>
             </div>
