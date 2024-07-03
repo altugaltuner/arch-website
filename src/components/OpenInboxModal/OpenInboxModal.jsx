8c3915c3c0d409f0f37c4687041ff781a1b46015
Altuğ Altuner <vefgend@gmail.com>
2024-07-03 15:54:01 +0300

    m

--- src/components/OpenInboxModal/OpenInboxModal.jsx
+++ src/components/OpenInboxModal/OpenInboxModal.jsx
@@ -1,7 +1,22 @@
 import React, { useState, useEffect } from "react";
 import "./OpenInboxModal.scss";
 
-function AboutMePage({ showInboxModal, setShowInboxModal }) {
+function OpenInboxModal({ showInboxModal, setShowInboxModal }) {
+    const [messages, setMessages] = useState([]);
+
+    useEffect(() => {
+        const fetchMessages = async () => {
+            try {
+                const response = await fetch('http://localhost:1337/api/multiple-messages/?populate=*');
+                const result = await response.json();
+                setMessages(result.data);
+            } catch (error) {
+                console.error("Error fetching messages:", error);
+            }
+        };
+
+        fetchMessages();
+    }, []);
 
     if (!showInboxModal) {
         return null;
@@ -12,15 +27,27 @@ function AboutMePage({ showInboxModal, setShowInboxModal }) {
             <div className="open-inbox-content">
                 <span className="open-inbox-modal-span" onClick={() => setShowInboxModal(false)}>X</span>
                 <h2 className='open-inbox-modal-header'>Gelen Kutusu</h2>
-
+                <div className="messages-container">
+                    {messages.map(message => (
+                        <div key={message.id} className="message">
+                            <h3 className="message-header">{message.attributes.header}</h3>
+                            <p className="message-content">{message.attributes.content}</p>
+                            {message.attributes.contentMedia.data.length > 0 && (
+                                <div className="message-media-div">
+                                    {message.attributes.contentMedia.data.map(media => (
+                                        <img className="message-media-img" key={media.id} src={media.attributes.url} alt={media.attributes.name} />
+                                    ))}
+                                </div>
+                            )}
+                        </div>
+                    ))}
+                </div>
                 <div className="open-inbox-modal-buttons">
-                    <button className='inbox-button-confirm' >Onayla</button>
                     <button className='inbox-button-cancel' onClick={() => setShowInboxModal(false)}>Kapat</button>
                 </div>
             </div>
         </div>
-
     );
 };
 
-export default AboutMePage;
+export default OpenInboxModal;
