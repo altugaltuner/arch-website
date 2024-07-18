619f01e5076b1ca7115ae6d8d68a48a21f561d65
Altuğ Altuner <vefgend@gmail.com>
2024-07-18 22:45:48 +0300

    m

--- src/components/ProjectCardsColumn/ProjectCardsColumn.jsx
+++ src/components/ProjectCardsColumn/ProjectCardsColumn.jsx
@@ -112,9 +112,7 @@ function ProjectCardsColumn({ companyProjects, roles, deleteModalOpen, setShowMo
                                 {project.attributes.projectCoverPhoto && project.attributes.projectCoverPhoto.data && (
                                     <img
                                         className="project-navbar-photos"
-                                        src={project.attributes.projectCoverPhoto.data.attributes.formats.thumbnail.url.startsWith('http')
-                                            ? project.attributes.projectCoverPhoto.data.attributes.formats.thumbnail.url
-                                            : `https://bold-animal-facf707bd9.strapiapp.com${project.attributes.projectCoverPhoto.data.attributes.formats.thumbnail.url}`}
+                                        src={project.attributes.projectCoverPhoto.data.attributes.formats.thumbnail.url}
                                         alt="Project Cover"
                                         onError={(e) => { console.log("Image Error:", e); }}
                                     />
