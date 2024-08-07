064d77bde8b2e878de3fd9c7722e0d28002372aa
Altuğ Altuner <vefgend@gmail.com>
2024-08-07 13:45:05 +0300

    m

--- src/pages/SurveysPage/SurveysPage.jsx
+++ src/pages/SurveysPage/SurveysPage.jsx
@@ -1,9 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./SurveysPage.scss";
 import { useAuth } from "../../components/AuthProvider";
 import Navigation from "../../components/Navigation/Navigation";
+import axios from "axios";
 
 function SurveysPage() {
+    const { user } = useAuth();
+    const [surveys, setSurveys] = useState([]);
+
+    const getSurveys = async () => {
+        try {
+            const response = await axios.get("https://bold-animal-facf707bd9.strapiapp.com/api/surveys?populate=*");
+            setSurveys(response.data.data);
+            console.log("response.data.data", response.data.data);
+        }
+        catch (error) {
+            console.error("Error fetching surveys:", error);
+        }
+    }
+
+    useEffect(() => {
+        getSurveys();
+    }, []);
 
     return (
         <main className="surveys-page-main">
@@ -11,24 +29,39 @@ function SurveysPage() {
             <div className="surveys-page-column">
                 <h1 className="div-big-header">Anketler</h1>
                 <div className="surveys-page-content">
-                    <div className="survey-one-div">
-                        <div className="survey-header-div">
-                            <p className="survey-creator-title">Anket Sahibi</p>
-                            <div className="survey-right-header">
-                                <p className="survey-category">Kategori</p>
-                                <p className="survey-date">Anket Tarihi</p>
+                    {surveys.map((survey) => (
+                        <div className="survey-one-div">
+                            <div className="survey-part">
+                                <div className="survey-header-div">
+                                    <p className="survey-creator-title">{survey.attributes.users_permissions_user.data.attributes.username}</p>
+                                    <div className="survey-right-header">
+                                        <p className="survey-category">{survey.attributes.category}</p>
+                                        <p className="survey-date">{survey.attributes.createdDate}</p>
+                                    </div>
+                                </div>
+                                <h3 className="survey-question">{survey.attributes.question}</h3>
+                                {survey.attributes.answers.answers.map((answer, index) => (
+                                    <div className="survey-answers" key={index}>
+                                        <p className="survey-answer">{answer.text}</p>
+                                        <p className="survey-answer-count">{answer.votes}</p>
+                                    </div>
+                                )
+                                )}
                             </div>
+                            <div className="survey-graph">
+                                <h1>Yorumlar</h1>
+                                {survey.attributes.comment && survey.attributes.comment.comments &&
+                                    survey.attributes.comment.comments.map((comment, index) => (
+                                        <div className="survey-comment" key={index}>
+                                            <p className="survey-comment-username">{comment.owner}</p>
+                                            <p className="survey-comment-text">{comment.text}</p>
+                                        </div>
+                                    ))
+                                }
+                            </div>
+                        </div>
+                    ))}
 
-                        </div>
-                        <h3 className="survey-question">Soru 1 e dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et </h3>
-                        <div className="survey-answers">
-                            <div className="survey-answer"><p className="survey-letter">A</p>Cevap 1 e dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla fac</div>
-                            <div className="survey-answer"><p className="survey-letter">B</p>Cevap 2 e dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla fac</div>
-                            <div className="survey-answer"><p className="survey-letter">C</p>Cevap 3 e dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla fac</div>
-                            <div className="survey-answer"><p className="survey-letter">D</p>Cevap 4 e dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla fac</div>
-                        </div>
-                        <div className="survey-graph"></div>
-                    </div>
                 </div>
             </div>
         </main>
